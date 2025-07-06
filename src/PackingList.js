c167440da7b481e5636585d055520ee79281e1be
nikhil2004nk <nikhil2004nk@gmail.com>
2025-07-06 19:58:21 +0530

    Initialize React project with Create React App, add essential files including package.json, .gitignore, and README.md. Implement core components for packing list functionality, including item management and statistics display. Add styling with CSS and set up basic project structure in public and src directories.

---
+++ src/PackingList.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Item } from "./Item";
+
+export function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems = [...items];
+  if (sortBy === "description")
+    sortedItems.sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems.sort((a, b) => Number(a.packed) - Number(b.packed));
+
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/Stats.js
@@ -0,0 +1,22 @@
+export function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list! 👜</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const packedItems = items.filter((item) => item.packed).length;
+  const percentage = Math.round((packedItems / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈️🏖️"
+          : `👜 You have ${numItems} items on your list, and already packed ${packedItems} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
